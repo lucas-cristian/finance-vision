5b1d8d928bb836bbdb22406cd673f8bf455584ee
Lucas Cristian <lucascristianbraz@yahoo.com>
2026-05-18 14:17:30 -0300

    feat: configura roteamento e estrutura principal da SPA

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import Home from './pages/Home.jsx'
+import Investimentos from './pages/Investimentos.jsx'
+import EducacaoFinanceira from './pages/EducacaoFinanceira.jsx'
+import ControleGastos from './pages/ControleGastos.jsx'
+import Contatos from './pages/Contatos.jsx'
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/investimentos" element={<Investimentos />} />
+          <Route path="/educacao-financeira" element={<EducacaoFinanceira />} />
+          <Route path="/controle-de-gastos" element={<ControleGastos />} />
+          <Route path="/contatos" element={<Contatos />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
+  )
+}
